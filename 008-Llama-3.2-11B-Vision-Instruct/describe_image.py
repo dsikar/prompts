435ee50a2f129d54c34c6e8d40ea15f2a659451d
Daniel Sikar <dsikar@gmail.com>
2025-02-13 10:12:58 +0000

    Describing image.

--- 008-Llama-3.2-11B-Vision-Instruct/describe_image.py
+++ 008-Llama-3.2-11B-Vision-Instruct/describe_image.py
@@ -24,13 +24,13 @@ processor = AutoProcessor.from_pretrained(
     token=hf_token
 )
 
-url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/0052a70beed5bf71b92610a43a52df6d286cd5f3/diffusers/rabbit.jpg"
+url = "https://raw.githubusercontent.com/dsikar/prompts/c5aec987d0c510b67d40c16d21e8bd500f1caaad/008-Llama-3.2-11B-Vision-Instruct/file.jpeg"
 image = Image.open(requests.get(url, stream=True).raw)
 
 messages = [
     {"role": "user", "content": [
         {"type": "image"},
-        {"type": "text", "text": "If I had to write a haiku for this one, it would be: "}
+        {"type": "text", "text": "Describe the image"}
     ]}
 ]
 input_text = processor.apply_chat_template(messages, add_generation_prompt=True)
